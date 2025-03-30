8aa3737172384c2e28b2f665997e1c32d836d754
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-03-30 19:51:44 -0300

    wip: components setup

--- src/components/Button/Button.interface.ts
+++ src/components/Button/Button.interface.ts
@@ -1,10 +1,16 @@
-import { ReactNode } from 'react';
+import { ComponentType, ReactElement, ReactNode } from 'react';
 import { TouchableOpacityProps } from 'react-native';
+import { SvgProps } from 'react-native-svg';
 
 export interface ButtonProps extends TouchableOpacityProps {
   variant: 'primary' | 'secondary' | 'social' | 'danger';
   disabled?: boolean;
   color?: string;
   onPress?: () => void;
+  hasIcon?: boolean;
+  icon?: {
+    size?: number;
+    component?: ComponentType<SvgProps> | ReactElement;
+  };
   children: ReactNode;
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,17 @@
+import Image from '../Image/Image';
 import { ButtonProps } from './Button.interface';
 import { ButtonText, StyledButton } from './Button.style';
 
-const Button = ({ children, variant, color, disabled, onPress }: ButtonProps) => {
+const Button = ({ children, variant, color, disabled, onPress, hasIcon, icon }: ButtonProps) => {
   return (
     <StyledButton variant={variant} color={color} disabled={disabled} onPress={onPress}>
+      {hasIcon && icon && (
+        <Image
+          imgHeight={icon?.size || 24}
+          imgWidth={icon?.size || 24}
+          svg={icon.component ?? (() => null)}
+        />
+      )}
       <ButtonText>{children}</ButtonText>
     </StyledButton>
   );
