1beea65d693cc9795403580db54c5b428041fc86
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-04-01 09:25:18 -0300

    feat: axios integration config

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,23 +1,25 @@
-import axios from 'axios'
-import { IAuthTokens, TokenRefreshRequest, applyAuthTokenInterceptor } from 'axios-jwt'
+import axios from 'axios';
+import { IAuthTokens, TokenRefreshRequest, applyAuthTokenInterceptor } from 'axios-jwt';
 
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = 'http://localhost:3000';
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000", // Ajustar conforme o backend
-    // headers: {"token": ""}
-})
+  baseURL: 'http://localhost:3000', // Ajustar conforme o backend
+  // headers: {"token": ""}
+});
 
 // Token utils
-export const refreshToken: TokenRefreshRequest = async (refreshToken: string): Promise<IAuthTokens | string> => {
-    const response = await axios.post(`${BASE_URL}/auth/refresh-token`, {
-        token: refreshToken
-    }) // Ajustar conforme o backend
+export const refreshToken: TokenRefreshRequest = async (
+  refreshToken: string
+): Promise<IAuthTokens | string> => {
+  const response = await axios.post(`${BASE_URL}/auth/refresh-token`, {
+    token: refreshToken,
+  }); // Ajustar conforme o backend
 
-    return {
-        accessToken: response.data.access_token,
-        refreshToken: response.data.refresh_token
-    }
-}
+  return {
+    accessToken: response.data.access_token,
+    refreshToken: response.data.refresh_token,
+  };
+};
 
-applyAuthTokenInterceptor(api, { requestRefresh: refreshToken })
+applyAuthTokenInterceptor(api, { requestRefresh: refreshToken });
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,22 +1,28 @@
-import { isLoggedIn, setAuthTokens, clearAuthTokens, getAccessToken, getRefreshToken } from 'axios-jwt'
-import { api } from './API'
-import { LoginBody } from './interfaces'
+import {
+  isLoggedIn,
+  setAuthTokens,
+  clearAuthTokens,
+  getAccessToken,
+  getRefreshToken,
+} from 'axios-jwt';
+import { api } from './API';
+import { LoginBody } from './interfaces';
 
 export const login = async (params: LoginBody) => {
-    const response = await api.post('/auth/login', params)
+  const response = await api.post('/auth/login', params);
 
-    setAuthTokens({
-        accessToken: response.data.access_token,
-        refreshToken: response.data.refresh_token
-    })
-}
+  setAuthTokens({
+    accessToken: response.data.access_token,
+    refreshToken: response.data.refresh_token,
+  });
+};
 
-export const logout = async () => await clearAuthTokens()
+export const logout = async () => await clearAuthTokens();
 
 if (await isLoggedIn()) {
-    // assume we are logged in because we have a refresh token
+  // assume we are logged in because we have a refresh token
 }
 
 // Get access to tokens
-export const accessToken = await getAccessToken()
-export const refreshToken = await getRefreshToken()
+export const accessToken = await getAccessToken();
+export const refreshToken = await getRefreshToken();
