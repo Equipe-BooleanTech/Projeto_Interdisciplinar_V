450bc560bad18d96bd3f43521dac2562a6a853f0
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-04-01 09:08:40 -0300

    feat: axios integration config

---
+++ src/services/API.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { IAuthTokens, TokenRefreshRequest, applyAuthTokenInterceptor } from 'axios-jwt'
+
+export const BASE_URL = "http://localhost:3000"
+
+export const api = axios.create({
+    baseURL: "http://localhost:3000", // Ajustar conforme o backend
+    // headers: {"token": ""}
+})
+
+// Token utils
+export const refreshToken: TokenRefreshRequest = async (refreshToken: string): Promise<IAuthTokens | string> => {
+    const response = await axios.post(`${BASE_URL}/auth/refresh-token`, {
+        token: refreshToken
+    }) // Ajustar conforme o backend
+
+    return {
+        accessToken: response.data.access_token,
+        refreshToken: response.data.refresh_token
+    }
+}
+
+applyAuthTokenInterceptor(api, { requestRefresh: refreshToken })
